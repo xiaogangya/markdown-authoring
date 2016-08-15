0a5c694236761e580e687d712003e5fe61a0b4ec
yanz <yanz@microsoft.com>
2016-08-15 17:35:49 +0800

    add validation when edit

--- src/commands/linkCommand.ts
+++ src/commands/linkCommand.ts
@@ -6,7 +6,7 @@ import Markdown from '../utilities/markdown'
 export default class LinkCommand {
   static linkLocationList = new Array<vscode.Location>()
 
-  static check(): Thenable<void> {
+  static checkAll(): Thenable<void> {
     LinkCommand.linkLocationList = new Array<vscode.Location>();
 
     return vscode.workspace.findFiles('**/*.md', '').then(uriList => {
@@ -14,24 +14,30 @@ export default class LinkCommand {
         return vscode.workspace.openTextDocument(uri);
       })).then(documentList => {
         documentList.forEach(document => {
-          const text = document.getText();
-          var positionList = Markdown.getLinkPositionList(text);
-          positionList = positionList.filter((position: any): boolean => {
-            var filePath = path.normalize(path.join(document.uri.fsPath, position.url));
-            return !fs.existsSync(filePath);
-          })
-
-          positionList.forEach(position => {
-            LinkCommand.linkLocationList.push(new vscode.Location(
-              document.uri,
-              new vscode.Range(
-                new vscode.Position(position.rowNum, position.columnNumStart), 
-                new vscode.Position(position.rowNum, position.columnNumEnd))
-            ))
-          })
+          var postions = LinkCommand.check(document);
+          LinkCommand.linkLocationList = LinkCommand.linkLocationList.concat(postions);
         })
       })
     })
   }
-  
+
+  static check(document: vscode.TextDocument): Array<vscode.Location> {
+    const text = document.getText();
+    var positionList = Markdown.getLinkPositionList(text);
+    positionList = positionList.filter((position: any): boolean => {
+      var filePath = path.normalize(path.join(document.uri.fsPath, position.url));
+      return !fs.existsSync(filePath);
+    })
+
+    let res = new Array<vscode.Location>();
+    positionList.forEach(position => {
+      res.push(new vscode.Location(
+        document.uri,
+        new vscode.Range(
+          new vscode.Position(position.rowNum, position.columnNumStart), 
+          new vscode.Position(position.rowNum, position.columnNumEnd))
+      ))
+    })
+    return res;
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
     const commandRegistration = vscode.commands.registerCommand('extension.checkLinks', () => {
         vscode.window.showInformationMessage('Check Links Start...');
 
-        return LinkCommand.check().then(() => {
+        return LinkCommand.checkAll().then(() => {
             const uri = vscode.Uri.parse(`${LinkCheckProvider.scheme}:Links.locations`);
             return vscode.workspace.openTextDocument(uri).then(doc => {
                 vscode.window.showTextDocument(doc, vscode.window.activeTextEditor.viewColumn + 1);
@@ -22,9 +22,56 @@ export function activate(context: vscode.ExtensionContext) {
         });
     });
 
+    validateWhenEdit(context);
+
     context.subscriptions.push(
         linkCheckProvider,
         providerRegistrations,
         commandRegistration
     );
+}
+
+function validateWhenEdit(context: vscode.ExtensionContext) {
+    var decorationType = vscode.window.createTextEditorDecorationType({
+		cursor: 'crosshair',
+		backgroundColor: 'rgba(255,0,0,0.3)'
+	});	
+
+    var activeEditor = vscode.window.activeTextEditor;
+	if (activeEditor) {
+		triggerValidation();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerValidation();	
+		}
+	}, null, context.subscriptions);
+	
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerValidation();
+		}
+	}, null, context.subscriptions);
+
+    var timeout = null;	
+	function triggerValidation() {
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(validate, 500);
+	}
+
+    function validate() {
+        if (activeEditor && activeEditor.document.languageId == 'markdown') {
+            var decorations : vscode.DecorationOptions[] = [];
+            let locations = LinkCommand.check(activeEditor.document);
+            locations.forEach(location => {
+                var decoration = { range: location.range, hoverMessage: '** bad link **'};
+                decorations.push(decoration);
+            })
+            activeEditor.setDecorations(decorationType, decorations);
+        }
+    }
 }
